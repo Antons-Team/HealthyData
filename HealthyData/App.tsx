438d098786a8346d8cbaeeddfa0c16bdf8724868
Joshua Morris <joshuajohnmorris@gmail.com>
2021-08-21 13:25:07 +1000

    tab navigation

--- HealthyData/App.tsx
+++ HealthyData/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './components/Home';
 import Example from './components/Example';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Example">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Example" component={Example} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Example" component={Example} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
