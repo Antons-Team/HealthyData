22cf1f8babd98bf60fc80e48506d1f82658c46d2
Joshua Morris <joshua.morris@polymathian.com>
2021-08-29 11:04:34 +1000

    Basic navigation on settings screen

--- HealthyData/@types/RootStackParams.tsx
+++ HealthyData/@types/RootStackParams.tsx
@@ -1,9 +1,6 @@
-import { NavigatorScreenParams } from "@react-navigation/native";
-import { MedicationsStackParamList } from "./MedicationsStackParamList";
-
 export type RootStackParamList = {
     Home: undefined;
     MedicationsNavigator: undefined;
-    Settings: undefined;
+    SettingsNavigator: undefined;
     Calendar: undefined;
 };
