8b64ace76448598836055bb2fea3639fa611147b
Joshua Morris <joshua.morris@polymathian.com>
2021-08-29 16:33:06 +1000

    Add scroll

--- HealthyData/components/AddMedicationInfo.tsx
+++ HealthyData/components/AddMedicationInfo.tsx
@@ -14,7 +14,7 @@ import { MedicationsStackParamList } from '../@types/MedicationsStackParamList';
 import { RouteProp } from '@react-navigation/native';
 import { MedicationItem } from '../@types/Schema';
 import {renderName} from '../utils/Display';
-import { TextInput } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
@@ -101,135 +101,137 @@ const AddMedicationInfo = ({navigation, route}: Props): JSX.Element => {
 
   return (
     <View style={styles.addMedicationContainer}>
-      <View style={styles.infoHeader}>
-        <Text style={styles.infoHeaderText}>{renderName(route.params.medication.name)} - {route.params.medication.dosage_amount}{route.params.medication.dosage_units}</Text>
-      </View>
-      
-      <Text style={styles.addMedicationTitle}>On which days are you to take {renderName(route.params.medication.brand_name)}?</Text>
+      <ScrollView>
+        <View style={styles.infoHeader}>
+          <Text style={styles.infoHeaderText}>{renderName(route.params.medication.name)} - {route.params.medication.dosage_amount}{route.params.medication.dosage_units}</Text>
+        </View>
+        
+        <Text style={styles.addMedicationTitle}>On which days are you to take {renderName(route.params.medication.brand_name)}?</Text>
 
-      <View style={styles.radioButtonsContainer}>
-        <RadioButton 
-          text='M' 
-          selected={days.monday} 
-          onPress={() => {
-            setDays({...days, monday: !days.monday});
-          }}
-        />
-        <RadioButton 
-          text='T' 
-          selected={days.tuesday} 
-          onPress={() => {
-            setDays({...days, tuesday: !days.tuesday});
-          }}
-        />
-        <RadioButton 
-          text='W' 
-          selected={days.wednesday} 
-          onPress={() => {
-            setDays({...days, wednesday: !days.wednesday});
-          }}
-        />
-        <RadioButton 
-          text='T' 
-          selected={days.thursday} 
-          onPress={() => {
-            setDays({...days, thursday: !days.thursday});
-          }}
-        />
-        <RadioButton 
-          text='F' 
-          selected={days.friday} 
-          onPress={() => {
-            setDays({...days, friday: !days.friday});
-          }}
-        />
-        <RadioButton 
-          text='S' 
-          selected={days.saturday} 
-          onPress={() => {
-            setDays({...days, saturday: !days.saturday});
-          }}
-        />
-        <RadioButton 
-          text='S' 
-          selected={days.sunday} 
-          onPress={() => {
-            setDays({...days, sunday: !days.sunday});
-          }}
-        />
-      </View>
+        <View style={styles.radioButtonsContainer}>
+          <RadioButton 
+            text='M' 
+            selected={days.monday} 
+            onPress={() => {
+              setDays({...days, monday: !days.monday});
+            }}
+          />
+          <RadioButton 
+            text='T' 
+            selected={days.tuesday} 
+            onPress={() => {
+              setDays({...days, tuesday: !days.tuesday});
+            }}
+          />
+          <RadioButton 
+            text='W' 
+            selected={days.wednesday} 
+            onPress={() => {
+              setDays({...days, wednesday: !days.wednesday});
+            }}
+          />
+          <RadioButton 
+            text='T' 
+            selected={days.thursday} 
+            onPress={() => {
+              setDays({...days, thursday: !days.thursday});
+            }}
+          />
+          <RadioButton 
+            text='F' 
+            selected={days.friday} 
+            onPress={() => {
+              setDays({...days, friday: !days.friday});
+            }}
+          />
+          <RadioButton 
+            text='S' 
+            selected={days.saturday} 
+            onPress={() => {
+              setDays({...days, saturday: !days.saturday});
+            }}
+          />
+          <RadioButton 
+            text='S' 
+            selected={days.sunday} 
+            onPress={() => {
+              setDays({...days, sunday: !days.sunday});
+            }}
+          />
+        </View>
 
-      <Text style={styles.addMedicationTitle}>What time of day are you to take {renderName(route.params.medication.brand_name)}?</Text>
+        <Text style={styles.addMedicationTitle}>What time of day are you to take {renderName(route.params.medication.brand_name)}?</Text>
 
-      <TouchableOpacity
-        onPress={() => {setShowTime(true);}}
-        style={styles.addMedicationEntry}
-      >
-        <Text style={styles.addMedicationTimeText}>{displayTime(timeOfDay)}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {setShowTime(true);}}
+          style={styles.addMedicationEntry}
+        >
+          <Text style={styles.addMedicationTimeText}>{displayTime(timeOfDay)}</Text>
+        </TouchableOpacity>
 
-      { (showTime &&
-        <RNDateTimePicker
-          testID="dateTimePicker"
-          value={timeOfDay}
-          mode='time'
-          is24Hour={false}
-          display='clock'
-          onChange={
-            (_, selectedDate) => {
-              if (selectedDate) {
-                setTimeOfDay(selectedDate);
+        { (showTime &&
+          <RNDateTimePicker
+            testID="dateTimePicker"
+            value={timeOfDay}
+            mode='time'
+            is24Hour={false}
+            display='clock'
+            onChange={
+              (_, selectedDate) => {
+                if (selectedDate) {
+                  setTimeOfDay(selectedDate);
+                }
+                setShowTime(false);
               }
-              setShowTime(false);
             }
-          }
-        />)
-      }
+          />)
+        }
 
-      <Text style={styles.addMedicationTitle}>When will you stop taking {renderName(route.params.medication.brand_name)}?</Text>
+        <Text style={styles.addMedicationTitle}>When will you stop taking {renderName(route.params.medication.brand_name)}?</Text>
 
-      <TouchableOpacity
-        onPress={() => {setShowDate(true);}}
-        style={styles.addMedicationEntry}
-      >
-        <Text style={styles.addMedicationTimeText}>{displayDate(date)}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {setShowDate(true);}}
+          style={styles.addMedicationEntry}
+        >
+          <Text style={styles.addMedicationTimeText}>{displayDate(date)}</Text>
+        </TouchableOpacity>
 
-      { (showDate &&
-        <RNDateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode='date'
-          is24Hour={false}
-          display='calendar'
-          onChange={
-            (_, selectedDate) => {
-              if (selectedDate) {
-                setDate(selectedDate);
+        { (showDate &&
+          <RNDateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode='date'
+            is24Hour={false}
+            display='calendar'
+            onChange={
+              (_, selectedDate) => {
+                if (selectedDate) {
+                  setDate(selectedDate);
+                }
+                setShowDate(false);
               }
-              setShowDate(false);
             }
-          }
-        />)
-      }
+          />)
+        }
 
-      <Text style={styles.addMedicationTitle}>How many doses of {renderName(route.params.medication.brand_name)} will you take?</Text>
+        <Text style={styles.addMedicationTitle}>How many doses of {renderName(route.params.medication.brand_name)} will you take?</Text>
 
-      <TextInput
-        style={styles.addMedicationEntry}
-        keyboardType='numeric'
-        value={doses}
-        onChangeText={setDoses}
-      />
+        <TextInput
+          style={styles.addMedicationEntry}
+          keyboardType='numeric'
+          value={doses}
+          onChangeText={setDoses}
+        />
 
-      <Text style={styles.addMedicationTitle}>What is your current supply of {renderName(route.params.medication.brand_name)}?</Text>
+        <Text style={styles.addMedicationTitle}>What is your current supply of {renderName(route.params.medication.brand_name)}?</Text>
 
-      <TextInput
-        style={styles.addMedicationEntry}
-        keyboardType='numeric'
-        value={supply}
-        onChangeText={setSupply}
-      />
+        <TextInput
+          style={styles.addMedicationEntry}
+          keyboardType='numeric'
+          value={supply}
+          onChangeText={setSupply}
+        />
+      </ScrollView>
 
       <View style={styles.infoButton}>
         <Button 
