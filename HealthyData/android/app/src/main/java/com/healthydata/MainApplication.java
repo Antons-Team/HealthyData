17c6ec4e7221abaead8bc5eec9d964be9b38b71d
yiwenjiang0 <yiwenjiang01@gmail.com>
2021-09-04 09:40:00 +1000

    install facebook sdk

--- HealthyData/android/app/src/main/java/com/healthydata/MainApplication.java
+++ HealthyData/android/app/src/main/java/com/healthydata/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.emeraldsanto.encryptedstorage.RNEncryptedStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          // packages.add(new FBSDKPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
