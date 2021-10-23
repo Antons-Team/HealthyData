c45228f8b664bd5418f543bdef71b26372714099
yiwenjiang0 <yiwenjiang01@gmail.com>
2021-10-24 05:46:03 +1000

    fixed android build error

--- HealthyData/android/app/src/main/java/com/healthydata/MainApplication.java
+++ HealthyData/android/app/src/main/java/com/healthydata/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.emeraldsanto.encryptedstorage.RNEncryptedStoragePackage;
