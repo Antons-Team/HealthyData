0d974987709ef0d1defd1fb37f569a5981c0ad43
yiwenjiang0 <yiwenjiang01@gmail.com>
2021-10-28 12:24:28 +1000

    .

--- HealthyData/App.tsx
+++ HealthyData/App.tsx
@@ -2,9 +2,6 @@ import React, {ReactElement} from 'react';
 import RootNavigator from './navigation/RootNavigator';
 import AuthProvider from './auth/provider';
 
-// import { LogBox } from 'react-native';
-// LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
-// LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const App = (): ReactElement => {
   return (
